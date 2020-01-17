4d8de706372fb66af3b7cba95893a7000e4431ff
Gregor Sturm <mail@gregor-sturm.de>
2020-01-17 15:24:14 +0100

    Fix path in tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,6 +2,4 @@ from sctcrpy._io import read_10x
 
 
 def test_read_10x():
-    anndata = read_10x(
-        "tests/data/10x/vdj_v1_hs_pbmc3_b_filtered_contig_annotations.csv"
-    )
+    anndata = read_10x("tests/data/10x/vdbg_141_tcr_filtered_contig_annotations.csv")
