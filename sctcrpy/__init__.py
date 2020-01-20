f035e1bc0c793a551633bc417290daa96417bcec
Gregor Sturm <mail@gregor-sturm.de>
2020-01-20 16:00:16 +0100

    Init docs

--- sctcrpy/__init__.py
+++ sctcrpy/__init__.py
@@ -4,7 +4,7 @@ from get_version import get_version
 __version__ = get_version(__file__)
 del get_version
 
-__author__ = "Gregor Sturm"
+__author__ = ", ".join(["Gregor Sturm", "Tamas Szabo"])
 
 from scanpy import AnnData, read_h5ad
 from ._io import read_10x, read_tracer
