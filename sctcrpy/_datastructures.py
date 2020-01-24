0ed177f9c8d8fc91f1508c2a6c0cde3eaf314232
Gregor Sturm <mail@gregor-sturm.de>
2020-01-24 15:36:38 +0100

    Draft data structures

---
+++ sctcrpy/_datastructures.py
@@ -0,0 +1,57 @@
+from typing import Literal
+
+
+class TcrChain:
+    def __init__(
+        self,
+        chain_type: Literal["TRA", "TRB", "other"],
+        *,
+        cdr3: str = None,
+        cdr3_nt: str = None,
+        expr: float = None,
+        expr_raw: float = None,
+        is_productive: bool = None,
+    ):
+        """Data structure for a T cell receptor chain. jj
+        
+        Parameters
+        ----------
+        chain_type 
+            Currently supported: ["TRA", "TRB", "other"]        
+        cdr3 
+            Amino acid sequence of the CDR3 region 
+        cd3_nt 
+            Nucleotide sequence fo the CDR3 region 
+        expr 
+            Normalized read count for the CDR3 region. 
+            Will be UMIs for 10x and TPM for SmartSeq2. 
+        expr_raw
+            Raw read count for the CDR3 regions.
+        is_productive 
+            Is the chain productive?
+        """
+        if chain_type not in ["TRA", "TRB", "other"]:
+            raise ValueError("Invalid chain type: {}".format(chain_type))
+
+        self.chain_type = chain_type
+        self.cdr3 = cdr3
+        self.cdr3_nt = cdr3_nt
+        self.expr = expr
+        self.expr_raw = expr_raw
+        self.is_productive = is_productive
+
+
+class TcrCell:
+    def __init__(self, cell_id: str):
+        self._cell_id = cell_id
+        self._chains = list()
+
+    def __repr__(self):
+        return "TcrCell {} with {} chains".format(self._cell_id, len(self._chains))
+
+    @property
+    def cell_id(self):
+        return self._cell_id
+
+    def add_chain(self, chain: TcrChain):
+        self._chains.append(chain)
