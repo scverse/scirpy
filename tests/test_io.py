474ee4121a26c1bddca278ddd991f56fec978915
Gregor Sturm <mail@gregor-sturm.de>
2020-01-30 13:37:10 +0100

    Move sample dataset to tutorial folder.
    Create minimal test dataset for testing IO functions.

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,9 +1,9 @@
 from sctcrpy import read_10x_vdj, read_tracer
 
 
-def test_read_10x():
-    anndata = read_10x_vdj("tests/data/10x/all_contig_annotations.json")
+def test_read_10x_example():
+    anndata = read_10x_vdj("tutorial/example_data/10x/all_contig_annotations.json")
 
 
-def test_read_tracer():
-    anndata = read_tracer("tests/data/tracer/tracer_100")
+def test_read_tracer_example():
+    anndata = read_tracer("tutorial/example_data/tracer/tracer_100")
