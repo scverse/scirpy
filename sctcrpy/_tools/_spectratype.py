35da5f1558353900286edcfdeff1d5f898eaad71
Gregor Sturm <mail@gregor-sturm.de>
2020-03-04 14:41:05 +0100

    Fix spectratype
    Former-commit-id: 1a4862710287a957c55f324466e905e8adba561d

--- sctcrpy/_tools/_spectratype.py
+++ sctcrpy/_tools/_spectratype.py
@@ -2,20 +2,16 @@ from anndata import AnnData
 from typing import Callable, Union, Collection
 import numpy as np
 import pandas as pd
-from .._util import _which_fractions, _is_na
+from .._util import _is_na, _normalize_counts
 
 
 def spectratype(
     adata: AnnData,
     groupby: str,
     *,
-    target_col: Collection = ("TRB_1_cdr3_len",),
-    fun: Callable = np.sum,
-    for_cells: Union[None, list, np.ndarray] = None,
+    target_col: Union[str, Collection] = "TRA_1_cdr3_len",
+    combine_fun: Callable = np.sum,
     fraction: Union[None, str, bool] = None,
-    fraction_base: Union[None, str] = None,
-    inplace: bool = True,
-    as_dict: bool = False,
 ) -> pd.DataFrame:
     """Show the distribution of CDR3 region lengths. 
 
@@ -27,76 +23,49 @@ def spectratype(
         AnnData object to work on.
     groupby
         Group by this column from `obs`. Samples or diagnosis for example.
-    fun
-        A function definining how the target columns should be merged 
-        (e.g. sum, mean, median, etc).  
     target_col
         Columns containing CDR3 lengths.        
-    for_cells
-        A whitelist of cells that should be included in the analysis. 
-        If not specified, cells with NaN values in the group definition columns 
-        will be ignored. When the tool is executed by the plotting function,
-         the whitelist is not updated.         
+    combine_fun
+        A function definining how the target columns should be merged 
+        (e.g. sum, mean, median, etc).  
     fraction
         If True, compute fractions of expanded clonotypes rather than reporting
         abosolute numbers. If a string is supplied, that should be the column name 
         of a grouping (e.g. samples). 
-    fraction_base
-        Sets the column used as a bsis for fraction calculation explicitely.
-        Overrides the column set by `fraction`, but gets
-        ignored if `fraction` is `False`. 
-    inplace
-        If True, the results are added to `adata.uns`. Otherwise it returns a dict
-        with the computed values. 
-    as_dict
-        If True, returns a dictionary instead of a dataframe. Useful for testing.
+
 
     Returns
     -------
-    Depending on the value of `inplace`, either returns a data frame 
-    or adds it to `adata.uns`. 
+    A DataFrame with spectratype information. 
     """
     if len(np.intersect1d(adata.obs.columns, target_col)) < 1:
         raise ValueError(
             "`target_col` not found in obs. Where do you store CDR3 length information?"
         )
 
-    # Check how fractions should be computed
-    fraction, fraction_base = _which_fractions(fraction, fraction_base, groupby)
-    target_col = pd.unique(target_col).tolist()
+    if isinstance(target_col, str):
+        target_col = [target_col]
+    else:
+        target_col = list(set(target_col))
+    tcr_obs = adata.obs.copy()
+    tcr_obs["lengths"] = tcr_obs.loc[:, target_col].apply(combine_fun, axis=1)
 
-    # Preproces the data table (remove unnecessary columns and rows)
-    if (for_cells is None) or (len(for_cells) < 2):
-        for_cells = np.intersect1d(
-            adata.obs.loc[~_is_na(adata.obs[fraction_base])].index.values,
-            adata.obs.loc[~_is_na(adata.obs[groupby])].index.values,
-        )
-    tcr_obs = adata.obs.loc[
-        for_cells, pd.unique([groupby, fraction_base] + target_col).tolist()
-    ]
-
-    # Merge target columns into one single column applying the desired function rowwise
-    tcr_obs["lengths"] = tcr_obs.loc[:, target_col].apply(fun, axis=1)
-
-    # Compute group sizes as a basis of fractions)
-    group_sizes = tcr_obs.loc[:, fraction_base].value_counts().to_dict()
+    na_mask = _is_na(tcr_obs[groupby]) | _is_na(tcr_obs["lengths"])
+    tcr_obs = tcr_obs.loc[~na_mask, :]
 
     # Calculate distribution of lengths in each group
     cdr3_lengths = (
-        tcr_obs.groupby(pd.unique([groupby, fraction_base, "lengths"]).tolist())
-        .size()
-        .reset_index(name="count")
+        tcr_obs.groupby([groupby, "lengths"]).size().reset_index(name="count")
     )
-    cdr3_lengths["groupsize"] = (
-        cdr3_lengths[fraction_base].map(group_sizes).astype("int32")
-    )
-    if fraction:
-        cdr3_lengths["count"] /= cdr3_lengths["groupsize"]
-    cdr3_lengths = cdr3_lengths.groupby([groupby, "lengths"]).sum().reset_index()
+
+    # normalize to fractions
+    scale_vector = _normalize_counts(tcr_obs, normalize=fraction, default_col=groupby)
+    cdr3_lengths["count"] /= scale_vector
+
     cdr3_lengths = cdr3_lengths.pivot(index="lengths", columns=groupby, values="count")
-    cdr3_lengths = cdr3_lengths.loc[range(int(tcr_obs["lengths"].max()) + 1), :].fillna(
-        value=0.0
-    )
+    cdr3_lengths = cdr3_lengths.reindex(
+        range(int(tcr_obs["lengths"].max()) + 1)
+    ).fillna(value=0.0)
 
     # By default, the most abundant group should be the first on the plot,
     # therefore we need their order
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -3,242 +3,13 @@ import sctcrpy as st
 from scanpy import AnnData
 import pytest
 import numpy.testing as npt
+import pandas.testing as pdt
 import numpy as np
 from sctcrpy._util import _get_from_uns
 
 
-def test_chain_pairing():
-    obs = pd.DataFrame.from_records(
-        [
-            ["False", "nan", "nan", "nan", "nan", "nan"],
-            ["True", "True", "AAAA", "BBBB", "CCCC", "DDDD"],
-            ["True", "False", "AAAA", "BBBB", "CCCC", "DDDD"],
-            ["True", "nan", "AAAA", "nan", "nan", "nan"],
-            ["True", "False", "AAAA", "nan", "CCCC", "nan"],
-            ["True", "False", "AAAA", "BBBB", "nan", "nan"],
-            ["True", "False", "AAAA", "BBBB", "CCCC", "nan"],
-            ["True", "False", "nan", "nan", "CCCC", "nan"],
-            ["True", "False", "nan", "nan", "CCCC", "DDDD"],
-            ["True", "False", "AAAA", "nan", "CCCC", "DDDD"],
-        ],
-        columns=[
-            "has_tcr",
-            "multi_chain",
-            "TRA_1_cdr3",
-            "TRA_2_cdr3",
-            "TRB_1_cdr3",
-            "TRB_2_cdr3",
-        ],
-    )
-    adata = AnnData(obs=obs)
-    res = st.tl.chain_pairing(adata, inplace=False)
-    npt.assert_equal(
-        res,
-        [
-            "No TCR",
-            "Multichain",
-            "Two full chains",
-            "Orphan alpha",
-            "Single pair",
-            "Orphan alpha",
-            "Extra alpha",
-            "Orphan beta",
-            "Orphan beta",
-            "Extra beta",
-        ],
-    )
-
-
-def test_clip_and_count_clonotypes():
-    obs = pd.DataFrame.from_records(
-        [
-            ["cell1", "A", "ct1"],
-            ["cell2", "A", "ct1"],
-            ["cell3", "A", "ct1"],
-            ["cell3", "A", "NaN"],
-            ["cell4", "B", "ct1"],
-            ["cell5", "B", "ct2"],
-            ["cell6", "B", "ct3"],
-            ["cell7", "B", "ct4"],
-            ["cell8", "B", "ct4"],
-        ],
-        columns=["cell_id", "group", "clonotype"],
-    ).set_index("cell_id")
-    adata = AnnData(obs=obs)
-
-    res = st.tl.clip_and_count(
-        adata, groupby="group", target_col="clonotype", clip_at=2, fraction=False
-    )
-    assert res == {"A": {"1": 0, ">= 2": 1}, "B": {"1": 3, ">= 2": 1}}
-
-    res_frac = st.tl.clip_and_count(
-        adata, groupby="group", target_col="clonotype", clip_at=2, fraction=True
-    )
-    assert res_frac == {"A": {"1": 0, ">= 2": 1.0}, "B": {"1": 0.75, ">= 2": 0.25}}
-
-    # check if target_col works
-    adata.obs["new_col"] = adata.obs["clonotype"]
-    adata.obs.drop("clonotype", axis="columns", inplace=True)
-
-    # check if it raises value error if target_col does not exist
-    with pytest.raises(ValueError):
-        st.tl.clip_and_count(
-            adata, groupby="group", target_col="clonotype", clip_at=2, fraction=False,
-        )
-
-
-def test_clip_and_count_convergence():
-    obs = {
-        "AAGGTTCCACCCAGTG-1": {
-            "TRA_1_cdr3": "CALSDPNTNAGKSTF",
-            "TRA_1_cdr3_nt": "TGTGCTCTGAGTGACCCTAACACCAATGCAGGCAAATCAACCTTT",
-            "sample": 3,
-        },
-        "ACTATCTAGGGCTTCC-1": {
-            "TRA_1_cdr3": "CAVDGGTSYGKLTF",
-            "TRA_1_cdr3_nt": "TGTGCCGTGGACGGTGGTACTAGCTATGGAAAGCTGACATTT",
-            "sample": 1,
-        },
-        "CAGTAACAGGCATGTG-1": {
-            "TRA_1_cdr3": "CAVRDSNYQLIW",
-            "TRA_1_cdr3_nt": "TGTGCTGTGAGAGATAGCAACTATCAGTTAATCTGG",
-            "sample": 1,
-        },
-        "CCTTACGGTCATCCCT-1": {
-            "TRA_1_cdr3": "CAVRDSNYQLIW",
-            "TRA_1_cdr3_nt": "TGTGCTGTGAGGGATAGCAACTATCAGTTAATCTGG",
-            "sample": 1,
-        },
-        "CGTCCATTCATAACCG-1": {
-            "TRA_1_cdr3": "CAASRNAGGTSYGKLTF",
-            "TRA_1_cdr3_nt": "TGTGCAGCAAGTCGCAATGCTGGTGGTACTAGCTATGGAAAGCTGACATTT",
-            "sample": 5,
-        },
-        "CTTAGGAAGGGCATGT-1": {
-            "TRA_1_cdr3": "CALSDPNTNAGKSTF",
-            "TRA_1_cdr3_nt": "TGTGCTCTGAGTGACCCTAACACCAATGCAGGCAAATCAACCTTT",
-            "sample": 1,
-        },
-        "GCAAACTGTTGATTGC-1": {
-            "TRA_1_cdr3": "CAVDGGTSYGKLTF",
-            "TRA_1_cdr3_nt": "TGTGCCGTGGATGGTGGTACTAGCTATGGAAAGCTGACATTT",
-            "sample": 1,
-        },
-        "GCTCCTACAAATTGCC-1": {
-            "TRA_1_cdr3": "CALSDPNTNAGKSTF",
-            "TRA_1_cdr3_nt": "TGTGCTCTGAGTGATCCCAACACCAATGCAGGCAAATCAACCTTT",
-            "sample": 3,
-        },
-        "GGAATAATCCGATATG-1": {
-            "TRA_1_cdr3": "CAASRNAGGTSYGKLTF",
-            "TRA_1_cdr3_nt": "TGTGCAGCAAGTAGGAATGCTGGTGGTACTAGCTATGGAAAGCTGACATTT",
-            "sample": 5,
-        },
-        "AAACCTGAGATAGCAT-1": {
-            "TRA_1_cdr3": "CAGGGSGTYKYIF",
-            "TRA_1_cdr3_nt": "TGTGCAGGGGGGGGCTCAGGAACCTACAAATACATCTTT",
-            "sample": 3,
-        },
-        "AAACCTGAGTACGCCC-1": {
-            "TRA_1_cdr3": "CAMRVGGSQGNLIF",
-            "TRA_1_cdr3_nt": "TGTGCAATGAGGGTCGGAGGAAGCCAAGGAAATCTCATCTTT",
-            "sample": 5,
-        },
-        "AAACCTGCATAGAAAC-1": {
-            "TRA_1_cdr3": "CAFMKPFTAGNQFYF",
-            "TRA_1_cdr3_nt": "TGTGCTTTCATGAAGCCTTTTACCGCCGGTAACCAGTTCTATTTT",
-            "sample": 5,
-        },
-        "AAACCTGGTCCGTTAA-1": {
-            "TRA_1_cdr3": "CALNTGGFKTIF",
-            "TRA_1_cdr3_nt": "TGTGCTCTCAATACTGGAGGCTTCAAAACTATCTTT",
-            "sample": 3,
-        },
-        "AAACCTGGTTTGTGTG-1": {
-            "TRA_1_cdr3": "CALRGGRDDKIIF",
-            "TRA_1_cdr3_nt": "TGTGCTCTGAGAGGGGGTAGAGATGACAAGATCATCTTT",
-            "sample": 3,
-        },
-    }
-    obs = pd.DataFrame.from_dict(obs, orient="index")
-    adata = AnnData(obs=obs)
-
-    # Check counts
-    res = st.tl.clip_and_count(
-        adata, target_col="TRA_1_cdr3", groupby="sample", fraction=False,
-    )
-    assert res == {
-        1: {"1": 1, "2": 2, ">= 3": 0},
-        3: {"1": 3, "2": 1, ">= 3": 0},
-        5: {"1": 2, "2": 1, ">= 3": 0},
-    }
-
-    # Check fractions
-    res = st.tl.clip_and_count(adata, target_col="TRA_1_cdr3", groupby="sample")
-    assert res == {
-        1: {"1": 0.3333333333333333, "2": 0.6666666666666666, ">= 3": 0.0},
-        3: {"1": 0.75, "2": 0.25, ">= 3": 0.0},
-        5: {"1": 0.6666666666666666, "2": 0.3333333333333333, ">= 3": 0.0},
-    }
-
-
-def test_define_clonotypes():
-    obs = pd.DataFrame.from_records(
-        [
-            ["cell1", "AAAA", "nan", "nan", "nan"],
-            ["cell2", "nan", "nan", "nan", "nan"],
-            ["cell3", "AAAA", "nan", "nan", "nan"],
-            ["cell4", "AAAA", "BBBB", "nan", "nan"],
-            ["cell5", "nan", "nan", "CCCC", "DDDD"],
-        ],
-        columns=["cell_id", "TRA_1_cdr3", "TRA_2_cdr3", "TRB_1_cdr3", "TRB_2_cdr3"],
-    ).set_index("cell_id")
-    adata = AnnData(obs=obs)
-
-    res = st.tl.define_clonotypes(adata, inplace=False)
-    npt.assert_equal(
-        # order is by alphabet: BBBB < nan
-        # we don't care about the order of numbers, so this is ok.
-        res,
-        ["clonotype_1", np.nan, "clonotype_1", "clonotype_0", "clonotype_2"],
-    )
-
-    res_primary_only = st.tl.define_clonotypes(
-        adata, flavor="primary_only", inplace=False
-    )
-    npt.assert_equal(
-        # order is by alphabet: BBBB < nan
-        # we don't care about the order of numbers, so this is ok.
-        res_primary_only,
-        ["clonotype_0", np.nan, "clonotype_0", "clonotype_0", "clonotype_1"],
-    )
-
-    # test inplace
-    st.tl.define_clonotypes(adata, key_added="clonotype_")
-    npt.assert_equal(res, adata.obs["clonotype_"].values)
-
-
-def test_alpha_diversity():
-    obs = pd.DataFrame.from_records(
-        [
-            ["cell1", "A", "ct1"],
-            ["cell2", "A", "ct1"],
-            ["cell3", "A", "ct1"],
-            ["cell3", "A", "NaN"],
-            ["cell4", "B", "ct1"],
-            ["cell5", "B", "ct2"],
-            ["cell6", "B", "ct3"],
-            ["cell7", "B", "ct4"],
-        ],
-        columns=["cell_id", "group", "clonotype_"],
-    ).set_index("cell_id")
-    adata = AnnData(obs=obs)
-
-    res = st.tl.alpha_diversity(adata, groupby="group", target_col="clonotype_")
-    assert res == {"A": 0, "B": 2}
-
-
-def test_spectratype():
+@pytest.fixture
+def adata_tra():
     obs = {
         "AAGGTTCCACCCAGTG-1": {
             "TRA_1_cdr3_len": 15.0,
@@ -355,65 +126,225 @@ def test_spectratype():
     }
     obs = pd.DataFrame.from_dict(obs, orient="index")
     adata = AnnData(obs=obs)
+    return adata
 
-    # Check numbers
-    res = st.tl.spectratype(
-        adata,
-        target_col="TRA_1_cdr3_len",
-        groupby="sample",
-        inplace=False,
-        as_dict=True,
-        fraction=False,
+
+def test_chain_pairing():
+    obs = pd.DataFrame.from_records(
+        [
+            ["False", "nan", "nan", "nan", "nan", "nan"],
+            ["True", "True", "AAAA", "BBBB", "CCCC", "DDDD"],
+            ["True", "False", "AAAA", "BBBB", "CCCC", "DDDD"],
+            ["True", "nan", "AAAA", "nan", "nan", "nan"],
+            ["True", "False", "AAAA", "nan", "CCCC", "nan"],
+            ["True", "False", "AAAA", "BBBB", "nan", "nan"],
+            ["True", "False", "AAAA", "BBBB", "CCCC", "nan"],
+            ["True", "False", "nan", "nan", "CCCC", "nan"],
+            ["True", "False", "nan", "nan", "CCCC", "DDDD"],
+            ["True", "False", "AAAA", "nan", "CCCC", "DDDD"],
+        ],
+        columns=[
+            "has_tcr",
+            "multi_chain",
+            "TRA_1_cdr3",
+            "TRA_2_cdr3",
+            "TRB_1_cdr3",
+            "TRB_2_cdr3",
+        ],
+    )
+    adata = AnnData(obs=obs)
+    res = st.tl.chain_pairing(adata, inplace=False)
+    npt.assert_equal(
+        res,
+        [
+            "No TCR",
+            "Multichain",
+            "Two full chains",
+            "Orphan alpha",
+            "Single pair",
+            "Orphan alpha",
+            "Extra alpha",
+            "Orphan beta",
+            "Orphan beta",
+            "Extra beta",
+        ],
+    )
+
+
+def test_clip_and_count_clonotypes():
+    obs = pd.DataFrame.from_records(
+        [
+            ["cell1", "A", "ct1"],
+            ["cell2", "A", "ct1"],
+            ["cell3", "A", "ct1"],
+            ["cell3", "A", "NaN"],
+            ["cell4", "B", "ct1"],
+            ["cell5", "B", "ct2"],
+            ["cell6", "B", "ct3"],
+            ["cell7", "B", "ct4"],
+            ["cell8", "B", "ct4"],
+        ],
+        columns=["cell_id", "group", "clonotype"],
+    ).set_index("cell_id")
+    adata = AnnData(obs=obs)
+
+    res = st.tl.clip_and_count(
+        adata, groupby="group", target_col="clonotype", clip_at=2, fraction=False
+    )
+    assert res == {"A": {"1": 0, ">= 2": 1}, "B": {"1": 3, ">= 2": 1}}
+
+    res_frac = st.tl.clip_and_count(
+        adata, groupby="group", target_col="clonotype", clip_at=2, fraction=True
+    )
+    assert res_frac == {"A": {"1": 0, ">= 2": 1.0}, "B": {"1": 0.75, ">= 2": 0.25}}
+
+    # check if target_col works
+    adata.obs["new_col"] = adata.obs["clonotype"]
+    adata.obs.drop("clonotype", axis="columns", inplace=True)
+
+    # check if it raises value error if target_col does not exist
+    with pytest.raises(ValueError):
+        st.tl.clip_and_count(
+            adata, groupby="group", target_col="clonotype", clip_at=2, fraction=False,
+        )
+
+
+def test_clip_and_count_convergence(adata_tra):
+    # Check counts
+    res = st.tl.clip_and_count(
+        adata_tra, target_col="TRA_1_cdr3", groupby="sample", fraction=False,
     )
     assert res == {
-        0: {1: 0.0, 3: 0.0, 5: 0.0},
-        1: {1: 0.0, 3: 0.0, 5: 0.0},
-        2: {1: 0.0, 3: 0.0, 5: 0.0},
-        3: {1: 0.0, 3: 0.0, 5: 0.0},
-        4: {1: 0.0, 3: 0.0, 5: 0.0},
-        5: {1: 0.0, 3: 0.0, 5: 0.0},
-        6: {1: 0.0, 3: 0.0, 5: 0.0},
-        7: {1: 0.0, 3: 0.0, 5: 0.0},
-        8: {1: 0.0, 3: 0.0, 5: 0.0},
-        9: {1: 0.0, 3: 0.0, 5: 0.0},
-        10: {1: 0.0, 3: 0.0, 5: 0.0},
-        11: {1: 0.0, 3: 0.0, 5: 0.0},
-        12: {1: 2.0, 3: 1.0, 5: 0.0},
-        13: {1: 0.0, 3: 2.0, 5: 0.0},
-        14: {1: 2.0, 3: 0.0, 5: 1.0},
-        15: {1: 1.0, 3: 2.0, 5: 1.0},
-        16: {1: 0.0, 3: 0.0, 5: 0.0},
-        17: {1: 0.0, 3: 0.0, 5: 2.0},
+        1: {"1": 1, "2": 2, ">= 3": 0},
+        3: {"1": 3, "2": 1, ">= 3": 0},
+        5: {"1": 2, "2": 1, ">= 3": 0},
     }
 
     # Check fractions
-    res = st.tl.spectratype(
-        adata,
-        target_col="TRA_1_cdr3_len",
-        groupby="sample",
-        inplace=False,
-        as_dict=True,
-    )
+    res = st.tl.clip_and_count(adata_tra, target_col="TRA_1_cdr3", groupby="sample")
     assert res == {
-        0: {1: 0.0, 3: 0.0, 5: 0.0},
-        1: {1: 0.0, 3: 0.0, 5: 0.0},
-        2: {1: 0.0, 3: 0.0, 5: 0.0},
-        3: {1: 0.0, 3: 0.0, 5: 0.0},
-        4: {1: 0.0, 3: 0.0, 5: 0.0},
-        5: {1: 0.0, 3: 0.0, 5: 0.0},
-        6: {1: 0.0, 3: 0.0, 5: 0.0},
-        7: {1: 0.0, 3: 0.0, 5: 0.0},
-        8: {1: 0.0, 3: 0.0, 5: 0.0},
-        9: {1: 0.0, 3: 0.0, 5: 0.0},
-        10: {1: 0.0, 3: 0.0, 5: 0.0},
-        11: {1: 0.0, 3: 0.0, 5: 0.0},
-        12: {1: 0.4, 3: 0.2, 5: 0.0},
-        13: {1: 0.0, 3: 0.4, 5: 0.0},
-        14: {1: 0.4, 3: 0.0, 5: 0.25},
-        15: {1: 0.2, 3: 0.4, 5: 0.25},
-        16: {1: 0.0, 3: 0.0, 5: 0.0},
-        17: {1: 0.0, 3: 0.0, 5: 0.5},
+        1: {"1": 0.3333333333333333, "2": 0.6666666666666666, ">= 3": 0.0},
+        3: {"1": 0.75, "2": 0.25, ">= 3": 0.0},
+        5: {"1": 0.6666666666666666, "2": 0.3333333333333333, ">= 3": 0.0},
     }
+
+
+def test_define_clonotypes():
+    obs = pd.DataFrame.from_records(
+        [
+            ["cell1", "AAAA", "nan", "nan", "nan"],
+            ["cell2", "nan", "nan", "nan", "nan"],
+            ["cell3", "AAAA", "nan", "nan", "nan"],
+            ["cell4", "AAAA", "BBBB", "nan", "nan"],
+            ["cell5", "nan", "nan", "CCCC", "DDDD"],
+        ],
+        columns=["cell_id", "TRA_1_cdr3", "TRA_2_cdr3", "TRB_1_cdr3", "TRB_2_cdr3"],
+    ).set_index("cell_id")
+    adata = AnnData(obs=obs)
+
+    res = st.tl.define_clonotypes(adata, inplace=False)
+    npt.assert_equal(
+        # order is by alphabet: BBBB < nan
+        # we don't care about the order of numbers, so this is ok.
+        res,
+        ["clonotype_1", np.nan, "clonotype_1", "clonotype_0", "clonotype_2"],
+    )
+
+    res_primary_only = st.tl.define_clonotypes(
+        adata, flavor="primary_only", inplace=False
+    )
+    npt.assert_equal(
+        # order is by alphabet: BBBB < nan
+        # we don't care about the order of numbers, so this is ok.
+        res_primary_only,
+        ["clonotype_0", np.nan, "clonotype_0", "clonotype_0", "clonotype_1"],
+    )
+
+    # test inplace
+    st.tl.define_clonotypes(adata, key_added="clonotype_")
+    npt.assert_equal(res, adata.obs["clonotype_"].values)
+
+
+def test_alpha_diversity():
+    obs = pd.DataFrame.from_records(
+        [
+            ["cell1", "A", "ct1"],
+            ["cell2", "A", "ct1"],
+            ["cell3", "A", "ct1"],
+            ["cell3", "A", "NaN"],
+            ["cell4", "B", "ct1"],
+            ["cell5", "B", "ct2"],
+            ["cell6", "B", "ct3"],
+            ["cell7", "B", "ct4"],
+        ],
+        columns=["cell_id", "group", "clonotype_"],
+    ).set_index("cell_id")
+    adata = AnnData(obs=obs)
+
+    res = st.tl.alpha_diversity(adata, groupby="group", target_col="clonotype_")
+    assert res == {"A": 0, "B": 2}
+
+
+def test_spectratype(adata_tra):
+    # Check numbers
+    res1 = st.tl.spectratype(
+        adata_tra, target_col="TRA_1_cdr3_len", groupby="sample", fraction=False,
+    )
+    res2 = st.tl.spectratype(
+        adata_tra, target_col=("TRA_1_cdr3_len",), groupby="sample", fraction=False,
+    )
+    expected_count = pd.DataFrame.from_dict(
+        {
+            0: {1: 0.0, 3: 0.0, 5: 0.0},
+            1: {1: 0.0, 3: 0.0, 5: 0.0},
+            2: {1: 0.0, 3: 0.0, 5: 0.0},
+            3: {1: 0.0, 3: 0.0, 5: 0.0},
+            4: {1: 0.0, 3: 0.0, 5: 0.0},
+            5: {1: 0.0, 3: 0.0, 5: 0.0},
+            6: {1: 0.0, 3: 0.0, 5: 0.0},
+            7: {1: 0.0, 3: 0.0, 5: 0.0},
+            8: {1: 0.0, 3: 0.0, 5: 0.0},
+            9: {1: 0.0, 3: 0.0, 5: 0.0},
+            10: {1: 0.0, 3: 0.0, 5: 0.0},
+            11: {1: 0.0, 3: 0.0, 5: 0.0},
+            12: {1: 2.0, 3: 1.0, 5: 0.0},
+            13: {1: 0.0, 3: 2.0, 5: 0.0},
+            14: {1: 2.0, 3: 0.0, 5: 1.0},
+            15: {1: 1.0, 3: 2.0, 5: 1.0},
+            16: {1: 0.0, 3: 0.0, 5: 0.0},
+            17: {1: 0.0, 3: 0.0, 5: 2.0},
+        },
+        orient="index",
+    )
+    npt.assert_equal(res1.values, expected_count.values)
+    npt.assert_equal(res2.values, expected_count.values)
+
+    # Check fractions
+    res = st.tl.spectratype(adata_tra, target_col="TRA_1_cdr3_len", groupby="sample",)
+    expected_frac = pd.DataFrame.from_dict(
+        {
+            0: {1: 0.0, 3: 0.0, 5: 0.0},
+            1: {1: 0.0, 3: 0.0, 5: 0.0},
+            2: {1: 0.0, 3: 0.0, 5: 0.0},
+            3: {1: 0.0, 3: 0.0, 5: 0.0},
+            4: {1: 0.0, 3: 0.0, 5: 0.0},
+            5: {1: 0.0, 3: 0.0, 5: 0.0},
+            6: {1: 0.0, 3: 0.0, 5: 0.0},
+            7: {1: 0.0, 3: 0.0, 5: 0.0},
+            8: {1: 0.0, 3: 0.0, 5: 0.0},
+            9: {1: 0.0, 3: 0.0, 5: 0.0},
+            10: {1: 0.0, 3: 0.0, 5: 0.0},
+            11: {1: 0.0, 3: 0.0, 5: 0.0},
+            12: {1: 0.4, 3: 0.2, 5: 0.0},
+            13: {1: 0.0, 3: 0.4, 5: 0.0},
+            14: {1: 0.4, 3: 0.0, 5: 0.25},
+            15: {1: 0.2, 3: 0.4, 5: 0.25},
+            16: {1: 0.0, 3: 0.0, 5: 0.0},
+            17: {1: 0.0, 3: 0.0, 5: 0.5},
+        },
+        orient="index",
+    )
+    npt.assert_equal(res.values, expected_count.values)
 
 
 def test_group_abundance():
