a3c9dc757d9ec02339b9e16c3cafa5022cd5c940
Gregor Sturm <mail@gregor-sturm.de>
2020-03-20 15:44:11 +0100

    Cleanup TCR dist
    * Remove UMAP code
    * Remove Kidera distance
    
    
    Former-commit-id: 486fb2980e65f372ce94ecb35c034d3712d6dac5

--- sctcrpy/_plotting/_tcr_dist.py
+++ sctcrpy/_plotting/_tcr_dist.py
@@ -5,12 +5,12 @@ import numpy as np
 from .._tools._tcr_dist import get_igraph_from_adjacency
 
 
-def clonotype_network(adata, color):
+def clonotype_network(adata, color, *, key="clonotype", obsm_key="X_clonotype_network"):
     """Plot the clonotype network"""
-    idx = np.where(~np.any(np.isnan(adata.obsm["X_clonotype_network"]), axis=1))[0]
-    adj = adata.uns["sctcrpy"]["connectivities"][idx, :][:, idx]
+    idx = np.where(~np.any(np.isnan(adata.obsm[obsm_key]), axis=1))[0]
+    adj = adata.uns["sctcrpy"][key + "_connectivities"][idx, :][:, idx]
     g = get_igraph_from_adjacency(adj)
-    layout = ig.Layout(adata.obsm["X_clonotype_network"][idx, :].tolist())
+    layout = ig.Layout(adata.obsm[obsm_key][idx, :].tolist())
     # return g, layout
     categories = adata.obs.iloc[idx, :][color].astype("category").cat.codes.values
     coloring = ig.VertexClustering(g, membership=categories)
