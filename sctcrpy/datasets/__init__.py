c24644368ef6825c2af7862c088bd28d15ea5d74
Gregor Sturm <mail@gregor-sturm.de>
2020-03-25 20:02:01 +0100

    Patch progress bar.

--- sctcrpy/datasets/__init__.py
+++ sctcrpy/datasets/__init__.py
@@ -4,8 +4,25 @@ from .._util import _doc_params, _read_to_str
 from scanpy.readwrite import read
 from scanpy import settings
 import os
+from contextlib import contextmanager
+import tqdm
+from tqdm import tqdm as tqdm_
 
 HERE = Path(__file__).parent
+
+
+@contextmanager
+def _monkey_path_tqdm(neighbors_key, edges_cmap=None):
+    """Monkey-patch tqdm.auto to allow download without ipywidgets installed. 
+    
+    See also https://github.com/theislab/scanpy/pull/1130.
+    """
+    tqdm_auto = tqdm.auto.tqdm
+    tqdm.auto.tqdm = tqdm_
+    try:
+        yield
+    finally:
+        tqdm.auto.tqdm = tqdm_auto
 
 
 @_doc_params(processing_code=_read_to_str(HERE / "_processing_scripts/wu2020.py"))
@@ -25,7 +42,8 @@ def wu2020() -> AnnData:
     # TODO host it on github or similar
     url = "https://www.dropbox.com/s/m48f3uveb2lrrbq/wu2020.h5ad?dl=1"
     filename = settings.datasetdir / "wu2020.h5ad"
-    adata = read(filename, backup_url=url)
+    with _monkey_path_tqdm():
+        adata = read(filename, backup_url=url)
     return adata
 
 
@@ -45,5 +63,6 @@ def wu2020_3k() -> AnnData:
     # TODO host it on github or similar
     url = "https://www.dropbox.com/s/dcfadomspl2pl8n/wu2020_3k.h5ad?dl=1"
     filename = settings.datasetdir / "wu2020_3k.h5ad"
-    adata = read(filename, backup_url=url)
+    with _monkey_path_tqdm():
+        adata = read(filename, backup_url=url)
     return adata
