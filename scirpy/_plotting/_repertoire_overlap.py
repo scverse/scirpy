25c087ba20f07a50d33cb22c005f64bd23e5f050
Tamas Szabo <szabogtamas@gmail.com>
2020-04-06 12:20:48 +0200

    Added clonotype imbalance feature from notebook

--- scirpy/_plotting/_repertoire_overlap.py
+++ scirpy/_plotting/_repertoire_overlap.py
@@ -12,7 +12,7 @@ from ._styling import style_axes, DEFAULT_FIG_KWS, _init_ax
 from ._base import ol_scatter
 
 
-def repertoire_overlap(
+def clonotype_imbalance(
     adata: AnnData,
     groupby: str,
     *,
