95556aafb338637ced782eb874d243cd38b54f22
Gregor Sturm <mail@gregor-sturm.de>
2020-04-10 17:14:34 +0200

    Update dataset urls to files hosted on Github (#104)
    * Update dataset urls to files hosted on Github 
    
    (as release assets)
    
    * Update __init__.py
    
    Former-commit-id: f6ebf76976d9f12fc14892b5ecb20f29ee9f6fd8

--- scirpy/datasets/__init__.py
+++ scirpy/datasets/__init__.py
@@ -40,7 +40,7 @@ def wu2020() -> AnnData:
     """
     # os.makedirs(settings.datasetdir, exist_ok=True)
     # TODO host it on github or similar
-    url = "https://www.dropbox.com/s/m48f3uveb2lrrbq/wu2020.h5ad?dl=1"
+    url = "https://github.com/icbi-lab/scirpy/releases/download/v0.1/wu2020.h5ad"
     filename = settings.datasetdir / "wu2020.h5ad"
     with _monkey_patch_tqdm():
         adata = read(filename, backup_url=url)
@@ -61,7 +61,7 @@ def wu2020_3k() -> AnnData:
     """
     # os.makedirs(settings.datasetdir, exist_ok=True)
     # TODO host it on github or similar
-    url = "https://www.dropbox.com/s/dcfadomspl2pl8n/wu2020_3k.h5ad?dl=1"
+    url = "https://github.com/icbi-lab/scirpy/releases/download/v0.1/wu2020_3k.h5ad"
     filename = settings.datasetdir / "wu2020_3k.h5ad"
     with _monkey_patch_tqdm():
         adata = read(filename, backup_url=url)
