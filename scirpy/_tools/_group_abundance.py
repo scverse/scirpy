b0455ed68371d7bbac19bab559b43452c5459c50
Gregor Sturm <mail@gregor-sturm.de>
2020-04-15 09:15:14 +0200

    Use observed=True for most groupby operations
    Former-commit-id: e5f328a711c9638eeb370bc2cc492918cd2cf813

--- scirpy/_tools/_group_abundance.py
+++ scirpy/_tools/_group_abundance.py
@@ -26,7 +26,8 @@ def _group_abundance(
     # Calculate distribution of lengths in each group. Use sum instead of count
     # to reflect weights
     group_counts = (
-        tcr_obs.groupby([groupby, target_col])["count", "weight"]
+        tcr_obs.loc[:, [groupby, target_col, "count", "weight"]]
+        .groupby([groupby, target_col], observed=True)
         .sum()
         .reset_index()
         .rename(columns={"weight": "weighted_count"})
