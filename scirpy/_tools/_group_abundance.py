0c9b07ef9b7ad510cb6e06aab9162a51a2ca861e
Gregor Sturm <mail@gregor-sturm.de>
2020-04-15 08:53:13 +0200

    Revert "Fix pandas future warning in group abundance"
    This reverts commit dce50e530ccbaca504cdfeef620df13130681b4f.

--- scirpy/_tools/_group_abundance.py
+++ scirpy/_tools/_group_abundance.py
@@ -26,8 +26,7 @@ def _group_abundance(
     # Calculate distribution of lengths in each group. Use sum instead of count
     # to reflect weights
     group_counts = (
-        tcr_obs.groupby([groupby, target_col])
-        .loc[:, ["count", "weight"]]
+        tcr_obs.groupby([groupby, target_col])["count", "weight"]
         .sum()
         .reset_index()
         .rename(columns={"weight": "weighted_count"})
