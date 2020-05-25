603493882dc6014e34d6af371789979d6017e8ac
Gregor Sturm <mail@gregor-sturm.de>
2020-05-25 22:14:25 +0200

    Boilerplate code for including apidoc images

--- docs/extensions/function_images.py
+++ docs/extensions/function_images.py
@@ -1,6 +1,6 @@
 """Images for plot functions
 
-Taken from scanpy (c) Philipp Angerer
+Adapted from scanpy (c) Philipp Angerer
 """
 from pathlib import Path
 from typing import List, Any
@@ -12,17 +12,28 @@ from sphinx.ext.autodoc import Options
 def insert_function_images(
     app: Sphinx, what: str, name: str, obj: Any, options: Options, lines: List[str],
 ):
-    path = app.config.api_dir / f"{name}.png"
+    for ext in ["png", "svg"]:
+        path = app.config.api_dir / f"{name}.{ext}"
+        if path.is_file():
+            break
+
     if what != "function" or not path.is_file():
         return
+
+    # all lines but the first will be ignored.
+    # Currently, the only way out I can see would be to use raw html.
+    # Sphinx doesn't handle copying images then, why we put them in the `_static` dir.
+    # This, ultimately, is a hack and a cleaner solution would be welcome.
     lines[0:0] = [
-        f".. image:: {path.name}",
-        "   :width: 200",
-        "   :align: right",
+        f""":raw:html:`{lines[0]}<br />`<img src="{app.config.api_rel_dir}/{path.name}" style="width: 300px" />`""",
         "",
     ]
 
 
 def setup(app: Sphinx):
+    # Directory to search for the images
     app.add_config_value("api_dir", Path(), "env")
+    # relative path to the directory with the images from the document where they are
+    # included.
+    app.add_config_value("api_rel_dir", "", "env")
     app.connect("autodoc-process-docstring", insert_function_images)
