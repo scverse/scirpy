6f7117da52219f96539042769530cffd5b7f4df7
Gregor Sturm <mail@gregor-sturm.de>
2020-05-31 20:41:49 +0200

    Add AIRR rearrangement file format support.
    Former-commit-id: a59c3219ea7dc7c956962e66f6cc31cab2886739

--- scirpy/io/__init__.py
+++ scirpy/io/__init__.py
@@ -1,3 +1,3 @@
 from scanpy import read_h5ad
-from ._io import read_10x_vdj, read_tracer, from_tcr_objs
+from ._io import read_10x_vdj, read_tracer, from_tcr_objs, read_airr
 from ._datastructures import TcrCell, TcrChain
