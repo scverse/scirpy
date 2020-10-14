b5c446b0868a5641a2925b3ffed4777898845a54
Gregor Sturm <mail@gregor-sturm.de>
2020-10-14 16:49:01 +0200

    Fix vdj_usage plot

--- scirpy/_plotting/_vdj_usage.py
+++ scirpy/_plotting/_vdj_usage.py
@@ -5,22 +5,31 @@ import numpy as np
 from ..util import _normalize_counts, _is_na
 from .styling import _init_ax
 from ..io import IrChain
+from itertools import islice
+from copy import deepcopy
+
+
+def _sanitize_gene_name(gene_text):
+    """Reformat a VDJ gene name to be displayed in the plot"""
+    for tmp_chain in IrChain.VALID_LOCI:
+        gene_text = gene_text.replace(tmp_chain, "")
+    return gene_text
 
 
 def vdj_usage(
     adata: AnnData,
     *,
     vdj_cols: Sequence = (
-        "IR_VJ_1_j_gene",
         "IR_VJ_1_v_gene",
+        "IR_VJ_1_j_gene",
         "IR_VDJ_1_v_gene",
         "IR_VDJ_1_d_gene",
         "IR_VDJ_1_j_gene",
     ),
     normalize_to: Union[bool, str, Sequence[float]] = False,
     ax: Union[plt.Axes, None] = None,
-    bar_clip: int = 5,
-    top_n: Union[None, int] = 10,
+    max_segments: int = 5,
+    max_ribbons: Union[None, int] = 10,
     barwidth: float = 0.4,
     draw_bars: bool = True,
     full_combination: bool = True,
@@ -45,12 +54,13 @@ def vdj_usage(
         in a small sample is weighted more than a cell in a large sample.
     ax
         Custom axis if needed.
-    bar_clip
-        The maximum number of stocks for bars (number of different V, D or J segments
+    max_segments
+        The maximum number of segments in a bar (number of different V, D or J segments
         that should be shown separately).
-    top_n
+    max_ribbons
         The maximum number of ribbons (individual VDJ combinations). If set to `None`,
-        all ribbons are drawn.
+        all ribbons are drawn. If `full_combination=False`, the max number of ribbons
+        is additionally constrained by the number of segments.
     barwidth
         Width of bars.
     draw_bars
@@ -83,76 +93,65 @@ def vdj_usage(
     if ax is None:
         ax = _init_ax(default_figargs)
 
-    if top_n is None:
-        top_n = df.shape[0]
-    bar_text_clip = bar_clip + 1
-    gene_tops, gene_colors = dict(), dict()
+    if max_ribbons is None:
+        max_ribbons = df.shape[0]
+
+    # Store segments and colors of segments for each column individually.
+    gene_tops = {col: dict() for col in vdj_cols}
+    gene_colors = {col: dict() for col in vdj_cols}
 
     # Draw a stacked bar for every gene loci and save positions on the bar
-    for i, col in enumerate(vdj_cols):
-        td = (
-            df.groupby(col)
+    for col_idx, col_name in enumerate(vdj_cols):
+        gene_weights = (
+            df.groupby(col_name)
             .agg({"cell_weights": "sum"})
             .sort_values(by="cell_weights", ascending=False)
             .reset_index()
         )
-        genes = td[col].tolist()
-        td = td["cell_weights"]
-        # IR_VDJ_1_v_gene -> VDJ1v
-        sector = "".join(col.split("_")[1:4])
+        genes = gene_weights[col_name].tolist()
+        segment_sizes = gene_weights["cell_weights"]
 
-        if full_combination:
-            unct = td[
-                bar_clip + 1 :,
-            ].sum()
-            if td.size > bar_clip:
-                if draw_bars:
-                    ax.bar(i + 1, unct, width=barwidth, color="grey", edgecolor="black")
-                gene_tops["other_" + sector] = unct
-                bottom = unct
-            else:
-                gene_tops["other_" + sector] = 0
-                bottom = 0
-            bar_clip = min(bar_clip, td.shape[0] - 1)
-        else:
-            bottom = 0
-            bar_clip = td.shape[0] - 1
-
-        for j in range(bar_clip + 1):
-            # try:
-            y = td[bar_clip - j]
-            gene = genes[bar_clip - j]
-            if _is_na(gene):
-                gene = "No_" + sector
-            gene_tops[gene] = bottom + y
-            if full_combination:
-                bcolor = "lightgrey"
-            else:
-                bcolor = None
+        # Draw "other genes" segment that sucks up the genes omitted due to
+        # `max_segments`.
+        if segment_sizes.size > max_segments:
+            other_genes_segment_size = segment_sizes[max_segments:].sum()
             if draw_bars:
-                barcoll = ax.bar(
-                    i + 1,
-                    y,
+                ax.bar(
+                    col_idx + 1,
+                    other_genes_segment_size,
                     width=barwidth,
-                    bottom=bottom,
-                    color=bcolor,
+                    color="lightgrey",
                     edgecolor="black",
                 )
-                gene_colors[gene] = barcoll.patches[-1].get_facecolor()
-                gene_text = gene
-                for tmp_chain in IrChain.VALID_LOCI:
-                    gene_text = gene_text.replace(tmp_chain, "")
-                if (bar_clip - bar_text_clip) < j:
-                    ax.text(
-                        1 + i - barwidth / 2 + 0.05,
-                        bottom + 0.05,
-                        gene_text,
-                    )
-            bottom += y
-        # except:
-        #     pass
+            gene_tops[col_name]["other"] = other_genes_segment_size
+            bottom = other_genes_segment_size
+        else:
+            gene_tops[col_name]["other"] = 0
+            bottom = 0
 
-    # return ax
+        # Draw gene segments
+        for segment_size, gene in list(zip(segment_sizes, genes))[:max_segments][::-1]:
+            if _is_na(gene):
+                gene = "none"
+            gene_tops[col_name][gene] = bottom + segment_size
+            if draw_bars:
+                bar_colors = ax.bar(
+                    col_idx + 1,
+                    segment_size,
+                    width=barwidth,
+                    bottom=bottom,
+                    color="lightgrey" if full_combination else None,
+                    edgecolor="black",
+                )
+                gene_colors[col_name][gene] = bar_colors.patches[-1].get_facecolor()
+
+                gene_text = _sanitize_gene_name(gene)
+                ax.text(
+                    1 + col_idx - barwidth / 2 + 0.05,
+                    bottom + 0.05,
+                    gene_text,
+                )
+            bottom += segment_size
 
     # Create a case for full combinations or just the neighbours
     if full_combination:
@@ -162,76 +161,84 @@ def vdj_usage(
         for lc in range(1, len(vdj_cols)):
             draw_mat.append(vdj_cols[lc - 1 : lc + 1])
 
-    init_n = 0
-    for target_pair in draw_mat:
-        gene_tops_c = gene_tops.copy()
-        init_n += 1
+    # Draw ribbons
+    for ribbon_start_x_coord, target_pair in enumerate(draw_mat, start=1):
+
+        def _add_source_weight(grp):
+            grp["source_weight"] = grp["cell_weights"].sum()
+            return grp
+
         # Count occurance of individual VDJ combinations
-        td = df.loc[:, target_pair + ["cell_weights"]]
-        td["genecombination"] = td.apply(
-            lambda x, y: "|".join([x[e] for e in y]), y=target_pair, axis=1
-        )
-        td = (
-            td.groupby("genecombination")
+        gene_combinations = (
+            df.groupby(target_pair)
             .agg({"cell_weights": "sum"})
-            .sort_values(by="cell_weights", ascending=False)
+            .groupby(target_pair[0])
+            .apply(_add_source_weight)
             .reset_index()
-        )
-        td["genecombination"] = td.apply(
-            lambda x: [x["cell_weights"]] + x["genecombination"].split("|"), axis=1
+            # Sort ribbons by: weight of the source segment, weight of the ribbon, name
+            # of the gene.
+            .sort_values(
+                by=["source_weight", "cell_weights", target_pair[0]],
+                ascending=[False, False, True],
+            )
         )
 
-        # Draw ribbons
-        for r in td["genecombination"][0 : top_n + 1]:
-            d = []
-            ht = r[0]
-            ribcol = None
-            for i in range(len(r) - 1):
-                g = r[i + 1]
-                # IR_VDJ_1_v_gene -> VDJ1v
-                sector = "".join(col.split("_")[1:4])
-                if _is_na(g):
-                    g = "No_" + sector
-                if g not in gene_tops_c:
-                    g = "other_" + sector
-                if not full_combination:
-                    if ribcol is None:
-                        ribcol = gene_colors[g]
-                t = gene_tops_c[g]
-                d.append([t - ht, t])
-                t = t - ht
-                gene_tops_c[g] = t
-            if draw_bars:
-                gapped_ribbons(
-                    d,
-                    ax=ax,
-                    gapwidth=barwidth,
-                    xstart=init_n + (barwidth / 2),
-                    ribcol=ribcol,
-                )
+        # need to work on a copy, otherwise the ribbons will appear shifted if
+        # full_combination=False
+        tmp_gene_tops = deepcopy(gene_tops)
+
+        for _, ribbon in gene_combinations.iloc[0:max_ribbons, :].iterrows():
+            ribbon_breakpoints = []
+            height = ribbon["cell_weights"]
+
+            # set ribbon color to the color of the first column in a column pair.
+            # In case of `full_combination`, assign new colors.
+            if full_combination:
+                ribbon_color = None
             else:
-                gapped_ribbons(
-                    d, ax=ax, gapwidth=0.1, xstart=init_n + 0.05, ribcol=ribcol
-                )
+                try:
+                    tmp_gene = ribbon[target_pair[0]]
+                    tmp_col = target_pair[0]
+                    tmp_gene = "none" if _is_na(tmp_gene) else tmp_gene
+                    ribbon_color = gene_colors[tmp_col][tmp_gene]
+                except KeyError:
+                    # Don't draw ribbon if the source gene is not drawn.
+                    continue
 
-    # Make tick labels nicer
-    # TODO this needs updating now that B cells are supported.
-    # ax.set_xticks(range(1, len(vdj_cols) + 1))
-    # if vdj_cols == [
-    #     "TRA_1_j_gene",
-    #     "TRA_1_v_gene",
-    #     "TRB_1_v_gene",
-    #     "TRB_1_d_gene",
-    #     "TRB_1_j_gene",
-    # ]:
-    #     ax.set_xticklabels(["TRAJ", "TRAV", "TRBV", "TRBD", "TRBJ"])
-    # else:
-    ax.set_xticklabels([x.replace("VJ_", "").replace("_gene", "") for x in vdj_cols])
+            for col_name in target_pair:
+                gene = ribbon[col_name]
+                if _is_na(gene):
+                    gene = "none"
+                if gene not in tmp_gene_tops[col_name]:
+                    gene = "other"
+                top = tmp_gene_tops[col_name][gene]
+                ribbon_breakpoints.append((top - height, top))
+                # this is in case there are multiple ribbons originating from
+                # a single segment.
+                tmp_gene_tops[col_name][gene] = top - height
+
+            if draw_bars:
+                gapwidth = barwidth
+                xstart = ribbon_start_x_coord + (barwidth / 2)
+            else:
+                gapwidth = 0.1
+                xstart = ribbon_start_x_coord + 0.05
+
+            _gapped_ribbons(
+                ribbon_breakpoints,
+                ax=ax,
+                gapwidth=gapwidth,
+                xstart=xstart,
+                ribcol=ribbon_color,
+            )
+
+    ax.set_xticks(range(1, len(vdj_cols) + 1))
+    ax.set_xticklabels([x.replace("IR_", "").replace("_gene", "") for x in vdj_cols])
 
     return ax
 
 
-def gapped_ribbons(
+def _gapped_ribbons(
     data: list,
     *,
     ax: Union[plt.axes, list, None] = None,
@@ -242,7 +249,7 @@ def gapped_ribbons(
     fun: Callable = lambda x: x[3]
     + (x[4] / (1 + np.exp(-((x[5] / x[2]) * (x[0] - x[1]))))),
     figsize: Tuple[float, float] = (3.44, 2.58),
-    figresolution: int = 300,
+    dpi: int = 300,
 ) -> plt.Axes:
     """Draws ribbons using `fill_between`
     Called by VDJ usage plot to connect bars.
@@ -250,25 +257,29 @@ def gapped_ribbons(
     Parameters
     ----------
     data
-        Breakpoints defining the ribbon as a 2D matrix. Each row is an x position, columns are the lower and upper extent of the ribbon at that position.
+        Breakpoints defining the ribbon as a 2D matrix. Each row is an x position,
+        columns are the lower and upper extent of the ribbon at that position.
     ax
         Custom axis, almost always called with an axis supplied.
     xstart
         The midpoint of the first bar.
     gapfreq
-        Frequency of bars. Normally a bar would be drawn at every integer x position, hence default is 1.
+        Frequency of bars. Normally a bar would be drawn at every integer x position,
+        hence default is 1.
     gapwidth
-        At every bar position, there will be a gap. The width of this gap is identical to bar widths, but could also be set to 0 if we need continous ribbons.
+        At every bar position, there will be a gap. The width of this gap is identical
+         to bar widths, but could also be set to 0 if we need continous ribbons.
     ribcol
         Face color of the ribbon.
     fun
-        A function defining the curve of each ribbon segment from breakpoint to breakpoint. By default, it is a sigmoid with 6 parameters:
-            range between x position of bars,
-            curve start on the x axis,
-            slope of curve,
-            curve start y position,
-            curve end y position,
-            compression factor of the sigmoid curve
+        A function defining the curve of each ribbon segment from breakpoint to
+        breakpoint. By default, it is a sigmoid with 6 parameters:
+         * range between x position of bars,
+         * curve start on the x axis,
+         * slope of curve,
+         * curve start y position,
+         * curve end y position,
+         * compression factor of the sigmoid curve
     figsize
         Size of the resulting figure in inches.
     figresolution
@@ -280,7 +291,7 @@ def gapped_ribbons(
     """
 
     if ax is None:
-        fig, ax = plt.subplots(figsize=figsize, dpi=figresolution)
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
     else:
         if isinstance(ax, list):
             ax = ax[0]
