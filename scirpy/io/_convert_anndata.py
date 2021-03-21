99e192e53631015a12c8b8a9c590005451ef1641
Gregor Sturm <mail@gregor-sturm.de>
2021-03-21 12:57:12 +0100

    WIP refactor data conversion

--- scirpy/io/_convert_anndata.py
+++ scirpy/io/_convert_anndata.py
@@ -1,34 +1,35 @@
 """Convert IrCells to AnnData and vice-versa"""
 import itertools
 from anndata import AnnData
-from ..util import _is_na, _is_true, _doc_params
+from ..util import _doc_params
 from ._common_doc import doc_working_model
-from ._datastructures import AirrCell, AirrChain
+from ._datastructures import AirrCell
 import pandas as pd
 from typing import Collection, List
 import numpy as np
 
-#: All fields available for each of the four IR chains.
-IR_OBS_KEYS = [
-    "locus",
-    "cdr3",
-    "cdr3_nt",
-    "expr",
-    "expr_raw",
-    "v_gene",
-    "d_gene",
-    "j_gene",
-    "c_gene",
-    "junction_ins",
-]
+# TODO Remove
+# #: All fields available for each of the four IR chains.
+# IR_OBS_KEYS = [
+#     "locus",
+#     "cdr3",
+#     "cdr3_nt",
+#     "expr",
+#     "expr_raw",
+#     "v_gene",
+#     "d_gene",
+#     "j_gene",
+#     "c_gene",
+#     "junction_ins",
+# ]
 
-#: All cols addedby scirpy when reading in data
-IR_OBS_COLS = [
-    f"IR_{chain_type}_{chain_id}_{key}"
-    for key, chain_type, chain_id in itertools.product(
-        IR_OBS_KEYS, ["VJ", "VDJ"], ["1", "2"]
-    )
-] + ["has_ir", "multi_chain"]
+# #: All cols addedby scirpy when reading in data
+# IR_OBS_COLS = [
+#     f"IR_{chain_type}_{chain_id}_{key}"
+#     for key, chain_type, chain_id in itertools.product(
+#         IR_OBS_KEYS, ["VJ", "VDJ"], ["1", "2"]
+#     )
+# ] + ["has_ir", "multi_chain"]
 
 
 def _sanitize_anndata(adata: AnnData) -> None:
@@ -38,19 +39,20 @@ def _sanitize_anndata(adata: AnnData) -> None:
         len(adata.X.shape) == 2
     ), "X needs to have dimensions, otherwise concat doesn't work. "
 
-    CATEGORICAL_COLS = ("locus", "v_gene", "d_gene", "j_gene", "c_gene", "multichain")
+    # TODO
+    # CATEGORICAL_COLS = ("locus", "v_gene", "d_gene", "j_gene", "c_gene", "multichain")
 
-    # Sanitize has_ir column into categorical
-    # This should always be a categorical with True / False
-    has_ir_mask = _is_true(adata.obs["has_ir"])
-    adata.obs["has_ir"] = pd.Categorical(
-        ["True" if x else "False" for x in has_ir_mask]
-    )
+    # # Sanitize has_ir column into categorical
+    # # This should always be a categorical with True / False
+    # has_ir_mask = _is_true(adata.obs["has_ir"])
+    # adata.obs["has_ir"] = pd.Categorical(
+    #     ["True" if x else "False" for x in has_ir_mask]
+    # )
 
-    # Turn other columns into categorical
-    for col in adata.obs.columns:
-        if col.endswith(CATEGORICAL_COLS):
-            adata.obs[col] = pd.Categorical(adata.obs[col])
+    # # Turn other columns into categorical
+    # for col in adata.obs.columns:
+    #     if col.endswith(CATEGORICAL_COLS):
+    #         adata.obs[col] = pd.Categorical(adata.obs[col])
 
     adata._sanitize()
 
@@ -76,91 +78,14 @@ def from_ir_objs(ir_objs: Collection[AirrCell]) -> AnnData:
 
     """
     ir_df = pd.DataFrame.from_records(
-        (_process_ir_cell(x) for x in ir_objs), index="cell_id"
+        (x.to_scirpy_record() for x in ir_objs), index="cell_id"
     )
     adata = AnnData(obs=ir_df, X=np.empty([ir_df.shape[0], 0]))
     _sanitize_anndata(adata)
     return adata
 
 
-@_doc_params(doc_working_model=doc_working_model)
-def _process_ir_cell(ir_obj: AirrCell) -> dict:
-    """\
-    Process a AirrCell object into a dictionary according
-    to our working model of adaptive immune receptors.
-
-    {doc_working_model}
-
-    Parameters
-    ----------
-    ir_obj
-        AirrCell object to process
-
-    Returns
-    -------
-    Dictionary representing one row of the final `AnnData.obs`
-    data frame.
-    """
-    res_dict = dict()
-    res_dict["cell_id"] = ir_obj.cell_id
-    res_dict["multi_chain"] = ir_obj.multi_chain
-    chain_dict = dict()
-    for junction_type in ["VJ", "VDJ"]:
-        # sorting subordinately by raw and cdr3 ensures consistency
-        # between load from json and load from csv.
-        tmp_chains = sorted(
-            [
-                x
-                for x in ir_obj.chains
-                if x.junction_type == junction_type and x.is_productive
-            ],
-            key=lambda x: (x.expr, x.expr_raw, x.cdr3),
-            reverse=True,
-        )
-        # multichain if at least one of the receptor arms is multichain
-        res_dict["multi_chain"] = res_dict["multi_chain"] | (len(tmp_chains) > 2)
-        # slice to max two chains
-        tmp_chains = tmp_chains[:2]
-        # add None if less than two chains
-        tmp_chains += [None] * (2 - len(tmp_chains))
-        chain_dict[junction_type] = tmp_chains
-
-    for key in IR_OBS_KEYS:
-        for junction_type, tmp_chains in chain_dict.items():
-            for i, chain in enumerate(tmp_chains):
-                res_dict["IR_{}_{}_{}".format(junction_type, i + 1, key)] = (
-                    getattr(chain, key) if chain is not None else None
-                )
-
-    # in some weird reasons, it can happen that a cell has been called from
-    # TCR-seq but no TCR seqs have been found. `has_ir` should be equal
-    # to "at least one productive chain"
-    res_dict["has_ir"] = not (
-        _is_na(res_dict["IR_VJ_1_cdr3"]) and _is_na(res_dict["IR_VDJ_1_cdr3"])
-    )
-
-    # if there are not chains at all, we want multi-chain to be nan
-    # This is to be consistent with when turning an anndata object into ir_objs
-    # and converting it back to anndata.
-    if not len(ir_obj.chains):
-        res_dict["multi_chain"] = np.nan
-
-    if _is_na(res_dict["IR_VJ_1_cdr3"]):
-        assert _is_na(
-            res_dict["IR_VJ_2_cdr3"]
-        ), "There can't be a secondary chain if there is no primary one: {}".format(
-            res_dict
-        )
-    if _is_na(res_dict["IR_VDJ_1_cdr3"]):
-        assert _is_na(
-            res_dict["IR_VDJ_2_cdr3"]
-        ), "There can't be a secondary chain if there is no primary one: {}".format(
-            res_dict
-        )
-
-    return res_dict
-
-
+# TODO function should be usable to retrieve the 'extra' chains, productive or not.
 def to_ir_objs(adata: AnnData) -> List[AirrCell]:
     """
     Convert an adata object with IR information back to a list of IrCells.
@@ -177,29 +102,21 @@ def to_ir_objs(adata: AnnData) -> List[AirrCell]:
     List of IrCells
     """
     cells = []
-    try:
-        for cell_id, row in adata.obs.iterrows():
-            tmp_ir_cell = AirrCell(cell_id, multi_chain=row["multi_chain"])
-            for chain_type, chain_id in itertools.product(["VJ", "VDJ"], ["1", "2"]):
-                # expr_raw is optional
-                keys = IR_OBS_KEYS[:]
-                if f"IR_{chain_type}_{chain_id}_expr_raw" not in row:
-                    keys.remove("expr_raw")
-                chain_dict = {
-                    key: row[f"IR_{chain_type}_{chain_id}_{key}"] for key in keys
-                }
-                # per definition, we currently only have productive chains in adata.
-                chain_dict["is_productive"] = True
-                if not _is_na(chain_dict["locus"]):
-                    # if no locus/chain specified, the correponding chain
-                    # does not exists and we don't want to add this.
-                    # This is also the way we can represent cells without IR.
-                    tmp_ir_cell.add_chain(AirrChain(**chain_dict))
 
-            cells.append(tmp_ir_cell)
-    except KeyError as e:
-        raise ValueError(
-            f"Key {str(e)} not found in adata. Does it contain immune receptor data?"
-        )
+    # TODO handle missing fields more gracefully?
+    ir_cols = adata.obs.columns[adata.obs.columns.str.startswith("IR_")]
+    for cell_id, row in adata.obs.iterrows():
+        tmp_ir_cell = AirrCell(cell_id, multi_chain=row["multi_chain"])
+        chains = {
+            (junction_type, chain_id): dict()
+            for junction_type, chain_id in itertools.product(["VJ", "VDJ"], ["1", "2"])
+        }
+        for tmp_col in ir_cols:
+            _, junction_type, chain_id, key = tmp_col.split("_")
+            chains[(junction_type, chain_id)][key] = row[tmp_col]
+
+        for tmp_chain in chains.values():
+            tmp_ir_cell.add_chain(tmp_chain)
+        tmp_ir_cell.add_serialized_chains(row["extra_chains"])
 
     return cells
--- scirpy/io/_datastructures.py
+++ scirpy/io/_datastructures.py
@@ -5,118 +5,13 @@ See also discussion at https://github.com/theislab/anndata/issues/115
 """
 
 from .._compat import Literal
-from ..util import _is_na, _is_true
-from typing import Union
-
-
-class AirrChain:
-    """Data structure for an Adaptive Immune Receptor Repertoire Chain.
-
-    The datastructure is compliant with the AIRR rearrangement schema v1.0.
-    An instance of this class represents a single row of the AIRR rearrangement schema.
-
-    Parameters
-    ----------
-    locus
-        IGMT locus name or "other".
-        See https://docs.airr-community.org/en/latest/datarep/rearrangements.html#locus-names.
-    cdr3
-        Amino acid sequence of the CDR3 region
-    cdr3_nt
-        Nucleotide sequence fo the CDR3 region
-    expr
-        Normalized read count for the CDR3 region.
-        Will be UMIs for 10x and TPM for SmartSeq2.
-    expr_raw
-        Raw read count for the CDR3 regions.
-    is_productive
-        Is the chain productive?
-    v_gene
-        gene symbol of v gene
-    d_gene
-        gene symbol of d gene
-    j_gene
-        gene symbol of j gene
-    c_gene
-        gene symbol of c gene
-    junction_ins
-        nucleotides inserted in the junctions.
-        For :term:`VJ<V(D)J>` chains: nucleotides inserted in the VJ junction
-        For :term:`VDJ<V(D)J>` chains: sum of nucleotides inserted in the VD + DJ junction
-    """
-
-    #: Chains with the :term:`V-J<V(D)J>` junction
-    VJ_LOCI = ("TRA", "TRG", "IGK", "IGL")
-
-    #: Chains with the :term:`V-D-J<V(D)J>` junction
-    VDJ_LOCI = ("TRB", "TRD", "IGH")
-
-    #: Valid chains are IMGT locus names or "other"
-    #: see https://docs.airr-community.org/en/latest/datarep/rearrangements.html#locus-names
-    VALID_LOCI = VJ_LOCI + VDJ_LOCI + ("other",)
-
-    # Attributes that are required to be equal for an object to be equal.
-    # Used for __eq__ and __hash__
-    _EQUALITY_ATTIBUTES = [
-        "locus",
-        "cdr3",
-        "cdr3_nt",
-        "expr",
-        "expr_raw",
-        "is_productive",
-        "v_gene",
-        "d_gene",
-        "j_gene",
-        "c_gene",
-        "junction_ins",
-    ]
-
-    def __init__(
-        self,
-        locus: Literal["TRA", "TRG", "IGK", "IGL", "TRB", "TRD", "IGH", "other"],
-        *,
-        cdr3: str = None,
-        cdr3_nt: str = None,
-        expr: float = None,
-        expr_raw: float = None,
-        is_productive: bool = None,
-        v_gene: str = None,
-        d_gene: str = None,
-        j_gene: str = None,
-        c_gene: str = None,
-        junction_ins: int = None,
-    ):
-        if locus not in self.VALID_LOCI:
-            raise ValueError("Invalid chain type: {}".format(locus))
-
-        self.locus = locus
-        self.junction_type = (
-            "VJ"
-            if locus in self.VJ_LOCI
-            else ("VDJ" if locus in self.VDJ_LOCI else None)
-        )
-        self.cdr3 = cdr3.upper() if not _is_na(cdr3) else None
-        self.cdr3_nt = cdr3_nt.upper() if not _is_na(cdr3_nt) else None
-        self.expr = float(expr)
-        self.expr_raw = float(expr_raw) if not _is_na(expr_raw) else None
-        self.is_productive = _is_true(is_productive)
-        self.v_gene = v_gene
-        self.d_gene = d_gene
-        self.j_gene = j_gene
-        self.c_gene = c_gene
-        self.junction_ins = int(junction_ins) if not _is_na(junction_ins) else None
-
-    def __repr__(self):
-        return "AirrChain object: " + str(self.__dict__)
-
-    def __eq__(self, other):
-        return all(
-            getattr(self, attr) == getattr(other, attr)
-            for attr in self._EQUALITY_ATTIBUTES
-        )
-
-    def __hash__(self):
-        return hash(tuple((getattr(self, attr) for attr in self._EQUALITY_ATTIBUTES)))
+from ..util import _is_na, _is_true, _doc_params
+from typing import Collection, Mapping, Union
+from airr import RearrangementSchema
+from scanpy import logging
+import json
+import numpy as np
+from ._common_doc import doc_working_model
 
 
 class AirrCell:
@@ -139,10 +34,26 @@ class AirrCell:
         regardless of the number of chains.
     """
 
+    #: Chains with the :term:`V-J<V(D)J>` junction
+    VJ_LOCI = ("TRA", "TRG", "IGK", "IGL")
+
+    #: Chains with the :term:`V-D-J<V(D)J>` junction
+    VDJ_LOCI = ("TRB", "TRD", "IGH")
+
+    # TODO remove?
+    #: Valid chains are IMGT locus names or "other"
+    #: see https://docs.airr-community.org/en/latest/datarep/rearrangements.html#locus-names
+    VALID_LOCI = VJ_LOCI + VDJ_LOCI + ("other",)
+
+    # attributes that are specific for the cell, not the chain, and should
+    # be the same for all chains of the same cell.
+    _CELL_ATTRIBUTES = ("is_cell",)  # non-standard field by 10x
+
     def __init__(self, cell_id: str, *, multi_chain: bool = False):
 
         self._cell_id = cell_id
-        self.multi_chain = _is_true(multi_chain)
+        self._multi_chain = _is_true(multi_chain)
+        self._fields = None
         self.chains = list()
 
     def __repr__(self):
@@ -152,6 +63,158 @@ class AirrCell:
     def cell_id(self):
         return self._cell_id
 
-    def add_chain(self, chain: AirrChain) -> None:
-        """Add a :class:`AirrChain`"""
+    def add_chain(self, chain: Mapping) -> None:
+        """Add a chain ot the cell.
+
+        A chain is a dictionary following
+        the `AIRR Rearrangement Schema <https://docs.airr-community.org/en/latest/datarep/rearrangements.html#productive>`__.
+        """
+        # TODO this should be `.validate_obj` but currently does not work
+        # because of https://github.com/airr-community/airr-standards/issues/508
+        RearrangementSchema.validate_header(chain.keys())
+        RearrangementSchema.validate_row(chain.keys())
+
+        if self._fields is None:
+            self._fields = list(chain.keys())
+        elif self._fields != list(chain.keys()):
+            raise ValueError("All chains must have the same fields!")
+
+        # TODO scirpy warning
+        if "locus" not in chain:
+            logging.warning(
+                "`locus` field not specified, but required for most scirpy functionality. "
+            )  # type: ignore
+        elif chain["locus"] not in self.VALID_LOCI:
+            logging.warning("scirpy only considers valid IGMT locus names. ")  # type: ignore
+
         self.chains.append(chain)
+
+    def add_serialized_chains(self, serialized_chains):
+        tmp_chains = json.loads(serialized_chains)
+        for chain in tmp_chains:
+            self.add_chain(chain)
+
+    def _split_chains(self):
+        """
+        Splits the chains into productive VJ, productive VDJ, and extra chains.
+
+        Returns
+        -------
+            dictionary with the following entries:
+              * `vj_chains`: The (up to) two most highly expressed, productive VJ-chains
+              * vdj_chains: The (up to) two most highly expressed, productive VDJ chains
+              * extra_chains: All remaining chains
+        """
+        is_multichain = self._multi_chain
+        split_chains = {"VJ": list(), "VDJ": list(), "extra": list()}
+        for tmp_chain in self.chains:
+            if "locus" not in tmp_chain:
+                split_chains["extra"].append(tmp_chain)
+            elif tmp_chain["locus"] in self.VJ_LOCI and tmp_chain["productive"]:
+                split_chains["VJ"].append(tmp_chain)
+            elif tmp_chain["locus"] in self.VDJ_LOCI and tmp_chain["productive"]:
+                split_chains["VDJ"].append(tmp_chain)
+            else:
+                split_chains["extra"].append(tmp_chain)
+
+        # TODO warning
+        # if (
+        #     "duplicate_count" not in self._fields
+        #     and "umi_count" not in self._fields
+        #     and "consensus_count" not in self._fields
+        # ):
+        #     logging.warning(
+        #         "No expression information available. Cannot rank chains by expression. "
+        #     )  # type: ignore
+
+        for junction_type in ["VJ", "VDJ"]:
+            split_chains[junction_type] = sorted(
+                split_chains[junction_type], key=self._key_sort_chains, reverse=True
+            )
+            # only keep the (up to) two most highly expressed chains
+            tmp_extra_chains = split_chains[junction_type][2:]
+            # if productive chains are appended to extra chains, it's a multichain cell!
+            is_multichain |= len(tmp_extra_chains)
+            split_chains["extra"] = tmp_extra_chains
+            split_chains[junction_type] = split_chains[junction_type][:2]
+
+        return is_multichain, split_chains
+
+    @staticmethod
+    def _key_sort_chains(chain):
+        """Get key to sort chains by expression"""
+        return (
+            chain.get("duplicate_count", 0),
+            chain.get("umi_count", 0),
+            chain.get("consensus_count", 0),
+            chain.get("junction", ""),
+            chain.get("junction_aa", ""),
+        )
+
+    @staticmethod
+    def _serialize_chains(chains):
+        """Serialize chains into a JSON object. This is useful for storing
+        an arbitrary number of extra chains in a single column of a dataframe."""
+        return json.dumps(chains)
+
+    # TODO should it be `include_fields` instead?
+    @_doc_params(doc_working_model=doc_working_model)
+    def to_scirpy_record(
+        self, drop_fields: Collection[str] = ("sequence", "sequence_aa")
+    ):
+        """Convert the cell to a scirpy record (i.e. one row of `adata.obs`) according
+        to our working model of adaptive immune receptors.
+
+        {doc_working_model}
+
+        Parameters
+        ----------
+        drop_fields
+            AIRR fields not to include into `adata.obs` (to save space and to not clutter
+            obs)
+        """
+        res_dict = dict()
+        res_dict["cell_id"] = self.cell_id
+        res_dict["multi_chain"], chain_dict = self._split_chains()
+        res_dict["extra_chains"] = self._serialize_chains(chain_dict.pop("extra"))
+
+        for key in self._fields:
+            for junction_type, tmp_chains in chain_dict.items():
+                for i in range(2):
+                    try:
+                        tmp_chain = tmp_chains[i]
+                    except IndexError:
+                        tmp_chain = dict()
+                    res_dict[
+                        "IR_{}_{}_{}".format(junction_type, i + 1, key)
+                    ] = tmp_chain.get(key, None)
+
+        # in some weird reasons, it can happen that a cell has been called from
+        # TCR-seq but no TCR seqs have been found. `has_ir` should be equal
+        # to "at least one productive chain"
+        res_dict["has_ir"] = not (
+            _is_na(res_dict["IR_VJ_1_cdr3"]) and _is_na(res_dict["IR_VDJ_1_cdr3"])
+        )
+
+        # if there are not chains at all, we want multi-chain to be nan
+        # This is to be consistent with when turning an anndata object into ir_objs
+        # and converting it back to anndata.
+        if not len(self.chains):
+            res_dict["multi_chain"] = np.nan
+
+        if _is_na(res_dict["IR_VJ_1_cdr3"]):
+            assert _is_na(
+                res_dict["IR_VJ_2_cdr3"]
+            ), "There can't be a secondary chain if there is no primary one: {}".format(
+                res_dict
+            )
+        if _is_na(res_dict["IR_VDJ_1_cdr3"]):
+            assert _is_na(
+                res_dict["IR_VDJ_2_cdr3"]
+            ), "There can't be a secondary chain if there is no primary one: {}".format(
+                res_dict
+            )
+
+        return res_dict
+
+        # TODO move .upper() to the functions that consume cdr3 sequences
