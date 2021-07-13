2c32b05fa847d9c0154fb6c55b9abd5a5a75f058
Gregor Sturm <mail@gregor-sturm.de>
2021-07-13 15:48:33 +0200

    Add testcase

--- scirpy/tests/test_io.py
+++ scirpy/tests/test_io.py
@@ -348,6 +348,12 @@ def test_read_tracer():
 
 
 @pytest.mark.conda
+def test_read_airr_issue280():
+    """Test that reading the example shown in issue #280 works."""
+    anndata = read_airr(TESTDATA / "airr" / "tra_issue_280.tsv")
+
+
+@pytest.mark.conda
 def test_read_airr():
     # Test that reading the files one-by-one or at once yields the same results
     anndata_tra = read_airr(TESTDATA / "airr/rearrangement_tra.tsv")
