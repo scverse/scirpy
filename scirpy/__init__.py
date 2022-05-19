0915fc6a1008d80a311783396d97554471559f0b
Gregor Sturm <mail@gregor-sturm.de>
2022-05-19 22:15:47 +0200

    Update pyproject.toml and  _metadata.py following the lead of anndata

--- scirpy/__init__.py
+++ scirpy/__init__.py
@@ -1,6 +1,6 @@
 """Python library for single-cell adaptive immune receptor repertoire (AIRR) analysis"""
 
-from ._metadata import __version__, __author__, __email__, within_flit
+from ._metadata import __version__, within_flit
 
 if not within_flit():
     from scanpy import AnnData, read_h5ad
--- scirpy/_metadata.py
+++ scirpy/_metadata.py
@@ -1,44 +1,45 @@
-"""Metadata. Adapted from https://github.com/theislab/scanpy/pull/1374/."""
 import traceback
 from pathlib import Path
 
 here = Path(__file__).parent
 
+
+def refresh_entry_points():
+    """\
+    Under some circumstances, (e.g. when installing a PEP 517 package via pip),
+    pkg_resources.working_set.entries is stale. This tries to fix that.
+    See https://github.com/pypa/setuptools_scm/issues/513
+    """
+    try:
+        import sys
+        import pkg_resources
+
+        ws: pkg_resources.WorkingSet = pkg_resources.working_set
+        for entry in sys.path:
+            ws.add_entry(entry)
+    except Exception:
+        pass
+
+
 try:
     from setuptools_scm import get_version
-    import pytoml
 
-    proj = pytoml.loads((here.parent / "pyproject.toml").read_text())
-    metadata = proj["tool"]["flit"]["metadata"]
-
-    # The `split` removes the "local" version part (PEP440), e.g. from
-    # 0.6.1.dev5+ga652c20.d20210130. Leaving the local part, results in the following
-    # error message:
-    #    WARNING: Built wheel for scirpy is invalid: Wheel has unexpected file name:
-    #    expected '0.6.1.dev5+ga652c20.d20210130', got '0.6.1.dev5-ga652c20.d20210130'
-    #
-    # TODO I expect this to be a temporary issue with either flit or wheel, as
-    # this has worked before.
-    __version__ = get_version(
-        # Allegedly, the parameters from pyproject.toml should be passed automatically.
-        # However, this didn't work, so I pass them explicitly here.
-        root="..",
-        relative_to=__file__,
-        **proj["tool"]["setuptools_scm"],
-    ).split("+")[0]
-    __author__ = metadata["author"]
-    __email__ = metadata["author-email"]
-
+    refresh_entry_points()
+    __version__ = get_version(root="..", relative_to=__file__)
 except (ImportError, LookupError, FileNotFoundError):
     from ._compat import pkg_metadata
 
     metadata = pkg_metadata(here.name)
     __version__ = metadata["Version"]
-    __author__ = metadata["Author"]
-    __email__ = metadata["Author-email"]
 
 
 def within_flit():
+    """\
+    Checks if we are being imported by flit.
+    This is necessary so flit can import __version__ without all depedencies installed.
+    There are a few options to make this hack unnecessary, see:
+    https://github.com/takluyver/flit/issues/253#issuecomment-737870438
+    """
     for frame in traceback.extract_stack():
         if frame.name == "get_docstring_and_version_via_import":
             return True
