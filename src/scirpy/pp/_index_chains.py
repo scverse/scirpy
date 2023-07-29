46c491645f8f026a74a0997ab5a92052db2fb808
Gregor Sturm <mail@gregor-sturm.de>
2023-07-29 19:33:54 +0200

    Add MWE for index_chains in numba

--- src/scirpy/pp/_index_chains.py
+++ src/scirpy/pp/_index_chains.py
@@ -4,12 +4,16 @@ from types import MappingProxyType
 from typing import Any, Callable, Union
 
 import awkward as ak
+import numba as nb
 from scanpy import logging
 
 from scirpy.io._datastructures import AirrCell
 from scirpy.util import DataHandler, _is_na2, tqdm
 
 SCIRPY_DUAL_IR_MODEL = "scirpy_dual_ir_v0.13"
+# make these constants available to numba
+_VJ_LOCI = tuple(AirrCell.VJ_LOCI)
+_VDJ_LOCI = tuple(AirrCell.VDJ_LOCI)
 
 
 @DataHandler.inject_param_docs()
@@ -133,6 +137,142 @@ def index_chains(
         "airr_key": airr_key,
         "sort_chains_by": str(sort_chains_by),
     }
+
+
+@DataHandler.inject_param_docs()
+def index_chains_numba(
+    adata: DataHandler.TYPE,
+    *,
+    filter: Union[Callable[[Mapping], bool], Sequence[Union[str, Callable[[Mapping], bool]]]] = (
+        "productive",
+        "require_junction_aa",
+    ),
+    sort_chains_by: Mapping[str, Any] = MappingProxyType(
+        {"duplicate_count": 0, "consensus_count": 0, "junction": "", "junction_aa": ""}
+    ),
+    airr_mod: str = "airr",
+    airr_key: str = "airr",
+    key_added: str = "chain_indices",
+) -> None:
+    """\
+    Selects primary/secondary VJ/VDJ cells per chain according to the :ref:`receptor-model`.
+
+    This function iterates through all chains stored in the :term:`awkward array` in
+    `adata.obsm[airr_key]` and
+
+     * labels chains as primary/secondary VJ/VDJ chains
+     * labels cells as multichain cells
+
+    based on the expression level of the chains and the specified filtering option.
+    By default, non-productive chains and chains without a valid CDR3 amino acid sequence are filtered out.
+
+    Additionally, chains without a valid IMGT locus are always filtered out.
+
+    For more details, please refer to the :ref:`receptor-model` and the :ref:`data structure <data-structure>`.
+
+    Parameters
+    ----------
+    {adata}
+    filter
+        Option to filter chains. Can be either
+          * a callback function that takes a chain-dictionary as input and returns a boolean (True to keep, False to discard)
+          * a list of "filtering presets". Possible values are `"productive"` and `"require_junction_aa"`.
+            `"productive"` removes non-productive chains and `"require_junction_aa"` removes chains that don't have
+            a CDR3 amino acid sequence.
+          * a list with a combination of both.
+
+        Multiple presets/functions are combined using `and`. Filtered chains do not count towards calling "multichain" cells.
+    sort_chains_by
+        A list of sort keys used to determine an ordering of chains. The chain with the highest value
+        of this tuple will be the primary chain, second-highest the secondary chain. If there are more chains, they
+        will not be indexed, and the cell receives the "multichain" flag.
+    {airr_mod}
+    {airr_key}
+    key_added
+        Key under which the chain indicies will be stored in `adata.obsm` and metadata will be stored in `adata.uns`.
+
+    Returns
+    -------
+    Nothing, but adds a dataframe to `adata.obsm[chain_indices]`
+    """
+    params = DataHandler(adata, airr_mod, airr_key)
+
+    # prepare filter functions
+    if isinstance(filter, Callable):
+        filter = [filter]
+    filter_presets = {
+        "productive": lambda x: x["productive"],
+        "require_junction_aa": lambda x: not _is_na2(x["junction_aa"]),
+    }
+    filter = [filter_presets[f] if isinstance(f, str) else f for f in filter]
+
+    # only warn if those fields are in the key (i.e. this should give a warning if those are missing with
+    # default settings. If the user specifies their own dictionary, they are on their own)
+    if "duplicate_count" in sort_chains_by and "consensus_count" in sort_chains_by:
+        if "duplicate_count" not in params.airr.fields and "consensus_count" not in params.airr.fields:
+            logging.warning("No expression information available. Cannot rank chains by expression. ")  # type: ignore
+
+    chain_index_awk = _index_chains_inner(params.airr, ak.ArrayBuilder()).snapshot()
+
+    return chain_index_awk
+
+    params.adata.obsm[key_added] = chain_index_awk  # type: ignore
+
+    # store metadata in .uns
+    params.adata.uns[key_added] = {
+        "model": SCIRPY_DUAL_IR_MODEL,  # can be used to distinguish different receptor models that may be added in the future.
+        "filter": str(filter),
+        "airr_key": airr_key,
+        "sort_chains_by": str(sort_chains_by),
+    }
+
+
+@nb.njit
+def _index_chains_inner(cells: ak.Array, ab: ak.ArrayBuilder):
+    for cell_chains in cells:
+        # Split chains into VJ and VDJ chains
+        vj_indices = nb.typed.List()
+        vdj_indices = nb.typed.List()
+        # TODO use prange
+        for i, tmp_chain in enumerate(cell_chains):
+            if True:  # all(f(tmp_chain) for f in filter) and "locus" in params.airr.fields:
+                if tmp_chain["locus"] in _VJ_LOCI:
+                    vj_indices.append(i)
+                elif tmp_chain["locus"] in _VDJ_LOCI:
+                    vdj_indices.append(i)
+
+        # Order chains by expression (or whatever was specified in sort_chains_by)
+        # TODO proper sorting
+        vj_indices = sorted(vj_indices)
+        vdj_indices = sorted(vdj_indices)
+
+        N_CHAINS = 2
+        n_vj = min(2, len(vj_indices))
+        n_vdj = min(2, len(vdj_indices))
+
+        # build awkward array with chain indices
+        ab.begin_record()
+        ab.field("VJ")
+        ab.begin_list()
+        # Add up to two chains
+        for i in vj_indices[:n_vj]:
+            ab.append(i)
+        # pad with None such thatwe have exactly 2 entries
+        for _ in range(N_CHAINS - n_vj):
+            ab.append(None)
+        ab.end_list()
+        ab.field("VDJ")
+        ab.begin_list()
+        for i in vdj_indices[:n_vdj]:
+            ab.append(i)
+        for _ in range(N_CHAINS - n_vdj):
+            ab.append(None)
+        ab.end_list()
+        ab.field("multichain")
+        ab.boolean(len(vj_indices) > 2 or len(vdj_indices) > 2)
+        ab.end_record()
+
+    return ab
 
 
 def _key_sort_chains(chains: list[Mapping], sort_chains_by: Mapping[str, Any], idx: int) -> Sequence:
