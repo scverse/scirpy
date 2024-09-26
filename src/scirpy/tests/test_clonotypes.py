8403a4067b27a9c7276e047dd53e99d43770fe30
felixpetschko <felixpetschko@gmail.com>
2024-09-26 14:37:40 +0200

    testing

--- src/scirpy/tests/test_clonotypes.py
+++ src/scirpy/tests/test_clonotypes.py
@@ -271,6 +271,7 @@ def test_clonotype_network(adata_conn, min_cells, min_nodes, layout, size_aware,
     npt.assert_almost_equal(coords.values, np.array(expected), decimal=1)
 
 
+@pytest.mark.extra
 def test_clonotype_network_mask_obs(adata_conn):
     
     expected = [
