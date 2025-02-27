fb0febdfe83dab704a4c02ff252b865f62100655
Gregor Sturm <mail@gregor-sturm.de>
2025-02-27 16:42:02 +0100

    Wrap up implementation

--- src/scirpy/tl/_mutational_load.py
+++ src/scirpy/tl/_mutational_load.py
@@ -1,20 +1,22 @@
 from collections.abc import Sequence
-from importlib.util import find_spec
 
 import awkward as ak
 import numba as nb
-import pandas as pd
 
 from scirpy.util import DataHandler
 
 
 @nb.njit
 def _hamming_distance(sequence: str | None, germline: str | None, ignore_chars: tuple[str]):
-    """Compute the hamming distance between two strings. Characters in `ignore_chars` are not counted towards the distance"""
+    """
+    Compute the hamming distance between two strings.
+
+     - Characters in `ignore_chars` are not counted towards the distance.
+     - Sequences are expected to be IMGT-aligned and of the same length. This is checked beforehand, therefore
+       we don't check it here.
+    """
     if sequence is None or germline is None:
         return None
-    if len(sequence) != len(germline):
-        raise ValueError("Sequences might not be IMGT aligned")
     ignore_chars: tuple[str] = (".", "N")
     distance = 0
     num_chars = len(sequence)
@@ -32,14 +34,19 @@ def _hamming_distance(sequence: str | None, germline: str | None, ignore_chars: 
     return distance
 
 
-@nb.njit(nb.types.Tuple((nb.optional(nb.int32), nb.optional(nb.int32)))(nb.types.unicode_type, nb.types.unicode_type))
+@nb.njit
 def _get_slice(region, junction):
-    """Get the start and end coordinates for a given region identifier.
+    """
+    Get the start and end coordinates for a given region identifier.
 
+    The coordinates for cdr3 and fwr4 depend depend on the junction length. Therefore
+    the junction sequence needs to be provided here.
     Reference: https://shazam.readthedocs.io/en/stable/topics/setRegionBoundaries/
+
+    -1 is a sentinel for None i.e., "slice from start" or "slice to end".
     """
     if region == "full":
-        return None, None
+        return -1, -1
     elif region == "v":
         return 0, 312
     elif region == "fwr1":
@@ -57,7 +64,7 @@ def _get_slice(region, junction):
         return 312, 312 + len(junction) - 6  # Adjusted based on junction length
     elif region == "fwr4":
         # FWR4 starts after the junction
-        return 312 + len(junction) - 6, None
+        return 312 + len(junction) - 6, -1
     else:
         raise ValueError("Invalid region")
 
@@ -72,13 +79,19 @@ def _apply_hamming_to_chains(
     arr_germline: ak.Array,
     arr_junction: ak.Array,
     ignore_chars: tuple[str],
-) -> ak.ArrayBuilder:
+) -> tuple[ak.ArrayBuilder]:
     """
-    Compute the (abolute) mutational load between IMGT aligned sequence and germline for all chains in the scirpy-formatted
-    AIRR Awkward array.
+    Compute the absolute and relative mutational load between IMGT aligned sequence and germline for a specified region.
 
-    `(start, end)` refer to a slice to select subregions of the alignment.
-    If `add_junction` is True, the length of the `junction` sequence is added to the `end` index.
+    `ab` and `ab_freq` are awkward array builders for the absolute and relative mutational load, respecitively.
+    They will have the same structure as `arr_sequence`, `arr_germline` and `arr_junction`.
+
+    `region` specifies the region, see `_get_slice` on how regions are transformed into coordinates.
+
+    `arr_sequence`, `arr_germline`, and `arr_junction` are awkward arrays with *identical* structure. They
+    are all slices of the scirpy formatted Awkward array in `adata.obsm`.
+
+    `ignore_chars` is a tuple of characters that are not considered when calculating the hamming distance.
     """
     assert len(arr_sequence) == len(arr_germline) == len(arr_junction)
     for r_seq, r_germ, r_junc in zip(  # noqa: B905 (`strict` not supported by numba)
@@ -87,12 +100,20 @@ def _apply_hamming_to_chains(
         assert len(r_seq) == len(r_germ) == len(r_junc)
         ab.begin_list(), ab_freq.begin_list()
         for seq, germline, junction in zip(r_seq, r_germ, r_junc):  # noqa: B905 (`strict` not supported by numba)
+            if len(seq) != len(germline):
+                raise ValueError(
+                    "Length aligned sequences length does not match. Are you sure sequences are IMGT aligned?"
+                )
             start, end = _get_slice(region, junction)
+            # workaround: numba doesn't support slicing with `None`, see https://github.com/numba/numba/issues/7948
+            # therefore setting to 0 == start and len(seq) == end
+            start = 0 if start < 0 else start
+            end = len(seq) if end < 0 else end
             seq_sliced = seq[start:end]
             germline_sliced = germline[start:end]
             dist = _hamming_distance(seq_sliced, germline_sliced, ignore_chars)
             ab.append(dist)
-            ab_freq.append(dist / len(seq_sliced))
+            ab_freq.append(None if dist is None else dist / len(seq_sliced))
         ab.end_list(), ab_freq.end_list()
     return ab, ab_freq
 
@@ -109,8 +130,7 @@ def mutational_load(
     germline_key: str = "germline_alignment_d_mask",
     junction_key: str = "junction",
     ignore_chars: Sequence[str] = (".", "N"),
-    frequency: bool = False,
-) -> None | pd.DataFrame:
+) -> None:
     """\
     Calculates observable mutation by comparing sequences with corresponding germline alignments and counting differences.
     Needs germline alignment information, which can be obtained by using the interoperability to Dandelion (https://sc-dandelion.readthedocs.io/en/latest/notebooks/5_dandelion_diversity_and_mutation-10x_data.html)
@@ -137,8 +157,6 @@ def mutational_load(
         * `"subregion"` - Similar to IMGT_V(D)J, but independent calculation of each subregion (CDR1-3 and FWR1-4, respectively)
     junction_col
         Awkward array key to access junction region information
-    frequency
-        If `True`, compute relative counts in addition to absolute counts (requires `pyarrow` as optional dependency).
     ignore_chars
         A list of characters to ignore while calculating differences. The default s to ignore the following:
         * `"N"` - masked or degraded nucleotide, i.e. D-segment is recommended to mask, because of lower sequence quality
@@ -152,21 +170,16 @@ def mutational_load(
     params = DataHandler(adata, airr_mod, airr_key, chain_idx_key)
     ignore_chars = tuple(ignore_chars)
 
-    if frequency:
-        if find_spec("pyarrow") is None:
-            raise ImportError(
-                "Calculating frequencies requires `pyarrow` as optional dependency. Run `pip install pyarrow` to install it."
-            ) from None
-
-    arrays = {
-        "arr_sequence": params.airr[sequence_key],
-        "arr_germline": params.airr[germline_key],
-        "arr_junction": params.airr[junction_key],
-        "ignore_chars": ignore_chars,
-    }
-
     for region in regions:
         key = "" if region == "full" else f"{region}_"
-        ab, ab_freq = _apply_hamming_to_chains(ak.ArrayBuilder(), ak.ArrayBuilder(), region, **arrays)
+        ab, ab_freq = _apply_hamming_to_chains(
+            ak.ArrayBuilder(),
+            ak.ArrayBuilder(),
+            region,
+            arr_sequence=params.airr[sequence_key],
+            arr_germline=params.airr[germline_key],
+            arr_junction=params.airr[junction_key],
+            ignore_chars=ignore_chars,
+        )
         params.airr[f"{key}mutation_count"] = ab.snapshot()
         params.airr[f"{key}mutation_freq"] = ab_freq.snapshot()
