13257632d4231a5d8d056f0483624546a5ca2961
Gregor Sturm <mail@gregor-sturm.de>
2025-08-04 20:18:47 +0200

    Reapply "Add documentation about required fields to from_airr_cells"
    This reverts commit 4d6b78d483f3c4317e1a3d6b24c07353e8966769.

--- src/scirpy/io/_convert_anndata.py
+++ src/scirpy/io/_convert_anndata.py
@@ -22,6 +22,8 @@ def from_airr_cells(airr_cells: Iterable[AirrCell], key_added: str = "airr") -> 
     This is useful for converting arbitrary data formats into
     the scirpy :ref:`data-structure`.
 
+    {doc_airr_fields}
+
     {doc_working_model}
 
     Parameters
