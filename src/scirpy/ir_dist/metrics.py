2f6399e5935f13c073d3c47f3ae300a938e149a3
felixpetschko <felixpetschko@gmail.com>
2026-07-16 12:26:30 +0200

    Do not allow cutoff=None anymore

--- src/scirpy/ir_dist/metrics.py
+++ src/scirpy/ir_dist/metrics.py
@@ -1618,7 +1618,7 @@ class NeedlemanWunschDistanceCalculator(_MetricDistanceCalculator):
 
     def __init__(
         self,
-        cutoff: int | None = 10,
+        cutoff: int = 10,
         *,
         gap_penalty: int = 11,
         n_jobs: int = -1,
@@ -1630,7 +1630,7 @@ class NeedlemanWunschDistanceCalculator(_MetricDistanceCalculator):
         if gap_penalty < 0:
             raise ValueError("`gap_penalty` must be non-negative.")
 
-        self.cutoff = 10 if cutoff is None else cutoff
+        self.cutoff = cutoff
         self.gap_penalty = gap_penalty
         self.histogram = histogram
 
